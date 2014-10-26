652a737168756df90c8add38a96b04bb42a3ba2f
jamesascarter <jamesascarter@gmail.com>
2014-10-26 15:33:06 +0000

    added sign in functionality

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -14,6 +14,12 @@ feature "User signs up" do
 		expect(page).to have_content("Sorry, your passwords don't match")
 	end
 
+	scenario "with an email that is already in the database" do
+		expect{ sign_up }.to change(User, :count).by(1)
+		expect{ sign_up }.to change(User, :count).by(0)
+		expect(page).to have_content("This email is already registered")
+	end
+
 
 
 
@@ -25,5 +31,39 @@ feature "User signs up" do
 		fill_in :password_confirmation, :with => password_confirmation
 		click_button 'Sign up'
 	end
+
+end
+
+feature "User signs in" do
+
+	before(:each) do
+		User.create(:email => 'test@test.com',
+					:password => 'test',
+					:password_confirmation => 'test')
+
+	end
+
+	scenario "with correct credentials" do
+		visit '/'
+		expect(page).not_to have_content("Welcome, test@test.com")
+		sign_in('test@test.com', 'test')
+		expect(page).to have_content("Welcome, test@test.com")
+	end
+
+	scenario "with incorrect credentials" do
+		visit '/'
+		expect(page).not_to have_content("Welcome, test@test.com")
+		sign_in('test@test.com', 'wrong')
+		expect(page).not_to have_content("Welcome, test@test.com")
+	end
+
+	def sign_in(email, password)
+		visit '/sessions/new'
+		fill_in "email", :with => email
+		fill_in 'password', :with => password
+		click_button 'Sign in'
+	end
+
+
 
 end
