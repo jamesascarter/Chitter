7e9105a97ec2cfc91ce9c66c3c37cbcd8b87c621
jamesascarter <jamesascarter@gmail.com>
2014-10-28 20:35:14 +0000

    Heroku deployment. Got rid of errors in datamapper file

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,8 +1,8 @@
 if ENV["RACK_ENV"] == "production"
-DataMapper.setup(:default, ENV[DATABASE_ENV])
+DataMapper.setup(:default, ENV["DATABASE_URL"])
 
 else
-	
+
 env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
